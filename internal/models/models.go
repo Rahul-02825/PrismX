1f0ce7b2e7d385ca538682c5ece301f0e7ad415e
Rahul-02825 <rahul2825.kannan@gmail.com>
2026-01-20 23:49:34 +0530

    fixing models

--- internal/models/models.go
+++ internal/models/models.go
@@ -2,6 +2,7 @@
 package models
 
 type User struct {
+	ID        string          `bson:"_id,omitempty" json:"id"`
 	Name     string             `bson:"name" json:"name"`
 	Password string             `bson:"password" json:"-"`
 }
